4e9cea91826628ac353eee9a3f30bc8703de8154
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2021-11-05 17:07:34 -0300

    Criando banco fake

--- src/domain/model/index.ts
+++ src/domain/model/index.ts
@@ -2,5 +2,23 @@ export type AnalyticsData = {
     totalAccessOnPages: {
         ignite: number;
         expertsClub: number;
+        all: number;
     };
+    totalAccessOnPagesByHour: {
+        ignite: number[];
+        expertsClub: number[];
+    };
+    totalAccessOnPagesByMonth: {
+        months: Array<number[]>;
+        days: number[];
+    };
+    accessGoals: number;
+    accessByDevice: {
+        mobile: number;
+        browser: number;
+    };
+    wordCloud: Array<{
+        name: string;
+        weight: number;
+    }>;
 };
