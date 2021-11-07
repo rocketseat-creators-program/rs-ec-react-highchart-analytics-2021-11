dc98193fa02fe30105cb3afd6eb99bd1c09aa4a3
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2021-11-07 14:39:17 -0300

    Criando componente TotalAccessOnPages

--- src/pages/AccessData/index.tsx
+++ src/pages/AccessData/index.tsx
@@ -1,11 +1,14 @@
 import Header from "../../components/Header";
 import { AnalyticsDataContextProvider } from "../../context/AnalyticsData/indext";
 import * as S from "./styles";
+import * as C from "./Components";
 
 const AccessData = () => (
     <AnalyticsDataContextProvider>
         <Header />
-        <S.Content />
+        <S.Content>
+            <C.TotalAccessOnPages />
+        </S.Content>
     </AnalyticsDataContextProvider>
 );
 
