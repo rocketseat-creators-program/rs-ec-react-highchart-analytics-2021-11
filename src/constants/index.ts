f67627f38b04f597e2d1b410505f16b397c8d437
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2021-11-07 15:32:16 -0300

    Criando grafico de total de acesso nas paginas

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,7 @@
 export const AnalyticsColors = {
-    black: "#191622;",
+    black: "#191622",
     grey: "#d1ced3",
-    lightGrey: "#F4F4F4;",
+    lightGrey: "#F4F4F4",
+    ecWine: "#DD2919",
+    purple: "#8257E5",
 };
--- src/pages/AccessData/Components/TotalAccessOnPages/index.tsx
+++ src/pages/AccessData/Components/TotalAccessOnPages/index.tsx
@@ -1,8 +1,78 @@
 import Chart from "../../../../components/Chart";
 import * as S from "./styles";
+import Eye from "../../../../assets/eye.svg";
+import { AnalyticsColors } from "../../../../constants";
+import useAnalyticsData from "../../../../hook/useAnalyticsData";
 
 export const TotalAccessOnPages = () => {
-    const options: Highcharts.Options = {};
+    const { analyticsDataState } = useAnalyticsData();
+
+    const options: Highcharts.Options = {
+        title: {
+            text: `<img src=${Eye} width="16" height="16" style="margin-right:8px;" alt="Ícone de view" /> Total de acessos nas páginas`,
+            useHTML: true,
+            align: "left",
+            style: {
+                padding: "30px",
+                fontSize: "20px",
+                color: AnalyticsColors.black,
+                fontWeight: "bold",
+                fontStyle: "Normal",
+            },
+        },
+
+        chart: {
+            type: "column",
+        },
+
+        series: [
+            {
+                name: "Ignite",
+                type: "column",
+                data: [analyticsDataState.totalAccessOnPages.ignite || 0],
+                color: AnalyticsColors.purple,
+            },
+            {
+                name: "Experts Club",
+                type: "column",
+                data: [analyticsDataState.totalAccessOnPages.expertsClub || 0],
+                color: AnalyticsColors.ecWine,
+            },
+        ],
+
+        tooltip: {
+            enabled: false,
+        },
+
+        yAxis: {
+            min: 0,
+            title: {
+                text: "",
+            },
+        },
+        plotOptions: {
+            column: {
+                dataLabels: {
+                    useHTML: true,
+                    enabled: true,
+                    color: AnalyticsColors.black,
+                    inside: true,
+                    verticalAlign: "bottom",
+                    borderWidth: 3,
+                    shadow: false,
+                    style: {
+                        fontSize: "36px",
+                        fontWeight: "bold",
+                        fontStyle: "Normal",
+                        lineHeight: "44px",
+                        fontFamily: "Inter",
+                        textOutline: "0",
+                        padding: "14px",
+                    },
+                },
+            },
+        },
+    };
 
     return (
         <S.Wrapper>
