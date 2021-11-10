95638e87533e00ba4c81f8a19df0ebbbf8624bba
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2021-11-10 17:12:18 -0300

    Ajustando header

---
+++ src/components/CustomChartTitle/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { AnalyticsColors } from "../../constants";
+
+export const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+
+    border: 1px solid ${AnalyticsColors.grey};
+    padding: 20px 32px;
+`;
+
+export const Title = styled.h5`
+    display: flex;
+    align-items: center;
+
+    font-family: Inter;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 24px;
+
+    color: ${AnalyticsColors.black};
+
+    margin: 5px 0px;
+`;
+export const Description = styled.span`
+    display: flex;
+    align-items: center;
+
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 17px;
+
+    color: #78787f;
+`;
