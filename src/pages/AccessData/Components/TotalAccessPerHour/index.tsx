d5bfca86bab20c35c8cf49905ef32299adbaf91d
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2021-11-10 16:49:36 -0300

    Criando o gráfico de total de acessos por hora

--- src/pages/AccessData/Components/TotalAccessPerHour/index.tsx
+++ src/pages/AccessData/Components/TotalAccessPerHour/index.tsx
@@ -1,8 +1,60 @@
+import { TooltipFormatterContextObject } from "highcharts";
 import Chart from "../../../../components/Chart";
+import { AnalyticsColors } from "../../../../constants";
+import useAnalyticsData from "../../../../hook/useAnalyticsData";
 import * as S from "./styles";
 
 const TotalAccessPerHour = () => {
-    const options: Highcharts.Options = {};
+    const { analyticsDataState } = useAnalyticsData();
+    const options: Highcharts.Options = {
+        title: {
+            text: "",
+        },
+
+        series: [
+            {
+                type: "column",
+                name: "Experts Club",
+                color: AnalyticsColors.ecWine,
+                data: analyticsDataState.totalAccessOnPagesByHour?.expertsClub,
+            },
+            {
+                type: "spline",
+                name: "Ignite",
+                color: AnalyticsColors.purple,
+                data: analyticsDataState.totalAccessOnPagesByHour?.ignite,
+            },
+        ],
+
+        xAxis: {
+            min: 0,
+            max: 23,
+            tickInterval: 1,
+        },
+
+        yAxis: {
+            title: {
+                text: "",
+            },
+        },
+
+        legend: {
+            align: "left",
+            verticalAlign: "top",
+            margin: 40,
+        },
+
+        tooltip: {
+            useHTML: true,
+            formatter() {
+                const self: TooltipFormatterContextObject = this;
+                return `<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; z-index: 1">
+                <h1 style="font-size: 30px; font-family: Inter; color: ${AnalyticsColors.black}; margin: 0px;"> ${self.point.y}</h1>
+                <span style="font-size: 16px; font-weight: 500; font-style: normal; color: ${AnalyticsColors.darkGray}"> Acessos </span>
+                </div>`;
+            },
+        },
+    };
     return (
         <S.Wrapper>
             <Chart options={options} />
