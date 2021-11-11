4861a00535985293c7e24ccbdab363d731f3ddf8
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2021-11-10 22:21:08 -0300

    Criando componente

--- src/pages/AccessData/Components/TotalAccessPerMonth/index.tsx
+++ src/pages/AccessData/Components/TotalAccessPerMonth/index.tsx
@@ -1,15 +1,56 @@
 import Chart from "../../../../components/Chart";
 import CustomChartTitle from "../../../../components/CustomChartTitle";
+import { AnalyticsColors } from "../../../../constants";
+import useAnalyticsData from "../../../../hook/useAnalyticsData";
 import * as S from "./styles";
 
 const TotalAccessPerMonth = () => {
-    const options: Highcharts.Options = {};
+    const { analyticsDataState } = useAnalyticsData();
+    const options: Highcharts.Options = {
+        title: {
+            text: "",
+        },
+
+        xAxis: {
+            type: "datetime",
+
+            title: {
+                text: "",
+            },
+
+            tickmarkPlacement: "on",
+            alignTicks: true,
+            labels: {
+                format: "{value: %b}",
+            },
+        },
+
+        yAxis: {
+            title: {
+                text: "",
+            },
+        },
+
+        series: [
+            {
+                type: "column",
+                name: "Total médio de acessos",
+                color: AnalyticsColors.purple,
+                data: analyticsDataState.totalAccessOnPagesByMonth?.months,
+            },
+        ],
+
+        legend: {
+            enabled: false,
+        },
+    };
     return (
         <S.Wrapper>
             <CustomChartTitle
                 description="Acompanhe a quantidade média de acesso por mês e dia nas páginas do ignite e do experts clubs"
                 title="Total médio de acessos por mês nas páginas"
             />
+
             <Chart options={options} />
         </S.Wrapper>
     );
